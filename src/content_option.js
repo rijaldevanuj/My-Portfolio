2a02742fc1f0f63bf6130592d2f958be54b8a2d2
Devanuj Rijal <rijaldevanuj@gmail.com>
2025-07-26 00:34:57 +0530

    Final Commit-- Project Done

--- src/content_option.js
+++ src/content_option.js
@@ -25,14 +25,9 @@ const worktimeline = [{
         date: "May-July 2025",
     },
     {
-        jobtitle: "Designer of week",
-        where: "Jamalya",
-        date: "2019",
-    },
-    {
-        jobtitle: "Designer of week",
-        where: "ALquds",
-        date: "2019",
+        jobtitle: "UX Design Intern",
+        where: "Book Your Caravan Pvt. Ltd.",
+        date: "Dec 2024 - Feb 2025",
     },
 ];
 
@@ -93,72 +88,56 @@ const services = [{
 ];
 
 const dataportfolio = [{
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "assets/images/WhatsApp Image 2025-06-12 at 11.01.57_0ede3a18.jpg",
+        description: "Remote Chemical Dosing Web Dashboard.",
+        link: "https://github.com/MACLEAI/PUMP_IOT",
     },
     {
-        img: "https://picsum.photos/400/800/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLXuD_SHK2YZht0uJTl2EOSggt4YsuqHPFtr0Iyrg2PVyAC9ZepE-9iigw2pRObL1rcXc&usqp=CAU",
+        description: "NITSMUN Website UI/UX Design & Development.",
+        link: "https://www.nitsmun.co.in/",
     },
     {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://images.unsplash.com/photo-1477949547006-b5ca1efd0f09?q=80&w=1174&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description: "Industrial Gas Leakge Detection System - IoT Project",
+        link: "https://github.com/rijaldevanuj/Gas-Leakage-Detection-System.git",
+    },
+   
+    {
+        img: "https://images.unsplash.com/photo-1470229722913-7c0e2dbbafd3?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description: "Incandescence 2025 - NIT Silchar; website UI/UX Design.",
+        link: "https://www.incand.in/",
     },
     {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://images.unsplash.com/photo-1618220179428-22790b461013?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fEludGVyaW9yJTIwZGVjb3J8ZW58MHx8MHx8fDA%3D",
+        description: "UX case study on Interio - A  luxury interior design firm.",
+        link: "https://www.behance.net/gallery/230257247/Interio-Crafted-Luxury-Spaces",
     },
 
+   
     {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://images.unsplash.com/photo-1547495827-fff7a5e8b26c?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description: "Tecnoesis 2024-NIT Silchar . Mobile App UI/UX Design.",
+        link: "https://play.google.com/store/apps/details?id=org.tecnoesis.tecnoapp&pcampaignid=web_share",
+    },
+  
+    {
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9zvTGQZ7GzCLBRMTP9l_6k03OUIbOozRuwA&s",
+        description: "This is a simple data analytics project made using python and its few libraries.",
+        link: "https://github.com/rijaldevanuj/Data-Analysis.git",
     },
     {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://plus.unsplash.com/premium_photo-1681398718759-f77c1d059141?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8YnJlYXN0JTIwY2FuY2VyfGVufDB8fDB8fHww",
+        description: "Breast Cancer Detection using Machine Learning",
+        link: "https://github.com/rijaldevanuj/Breast-Cancer-Detection-.git",
     },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/550/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
+  
 ];
 
 const contactConfig = {
     YOUR_EMAIL: "rijaldevanuj@gmail.com",
     YOUR_FONE: "+91 9394521290",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    description: "nterested in collaborating or have a project in mind? I’m always eager to meet new people and take on rewarding challenges. Reach out and let’s work together. ",
     // creat an emailjs.com account 
     // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
     YOUR_SERVICE_ID: "service_id",
